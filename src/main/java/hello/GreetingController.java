b6296ea159e5d01f7236cf36c071ca9beacd9197
Fabio Veronez <fabio.veronez@gmail.com>
2016-09-07 11:41:33 -0300

    First runnable code
    Add Resource, Controller and Spring Boot's Application classes.
    As well as `pom.xml`.
    
    This project's pom.xml file differs from one initially presented in Spring's [Getting Started guide](https://spring.io/guides/gs/rest-service/#use-maven)  in that it defines `<packaging>` to "war".
    
    Also add an initial `.gitlab-ci.yml` file that initially aims to execute maven's package command.

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import static java.lang.String.format;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), format(TEMPLATE, name));
+    }
+}
