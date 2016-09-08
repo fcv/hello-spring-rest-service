6b7044ce7d85e1063fff0a7a221ba800b95ab157
Fabio Veronez <fabio.veronez@gmail.com>
2016-09-08 10:10:40 -0300

    Add initial support to HATEOAS
    Add initial support to HATEOAS based on ["Building a Hypermedia-Driven RESTful Web Service"](https://spring.io/guides/gs/rest-hateoas/) getting started guide.

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,9 +1,14 @@
 package hello;
 
 import static java.lang.String.format;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +20,11 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), format(TEMPLATE, name));
+    public HttpEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        Greeting greeting = new Greeting(counter.incrementAndGet(), format(TEMPLATE, name));
+        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name))
+                .withSelfRel());
+        return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
 }
